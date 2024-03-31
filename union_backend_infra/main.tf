2cd42ef136cc949e44994be2e6a839c0e602ca5a
aniketrane040 <aniketrane0557@gmail.com>
2024-04-01 01:15:26 +0530

    union infra

--- union_backend_infra/main.tf
+++ union_backend_infra/main.tf
@@ -20,3 +20,63 @@ data "aws_ssm_parameter" "vpc-id" {
 data "aws_vpc" "selected" {
   id = data.aws_ssm_parameter.vpc-id.value
 }
+
+resource "aws_security_group" "union_sg" {
+  name = "union_sg_group"
+  vpc_id = data.aws_vpc.selected.id
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 3000
+    to_port = 3000
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecs_task_definition" "union_task_def" {
+  family = "union_task_def"
+  requires_compatibilities = ["FARGATE"]
+  container_definitions = jsondecode([
+    {
+        name = "union_backend_container"
+        image = "public.ecr.aws/m7p9x7q8/union-backend-app:latest"
+        cpu = "512"
+        memory = "512"
+        portMappings = [
+            {
+                container_port = 3000
+                host_port = 3000
+            }
+        ]
+        environment = [
+            {
+                name = "CONNECTION_URL"
+                value = ""
+            },
+            {
+                name = "PORT"
+                value = "3000"
+            }
+        ]
+    }
+  ])
+}
+
+resource "aws_ecs_cluster" "union_cluster" {
+  name = "union_cluster"
+}
+
+resource "aws_ecs_service" "union_service" {
+  name = "union_service"
+  cluster = aws_ecs_cluster.union_cluster.id
+  task_definition = aws_ecs_task_definition.union_task_def.arn
+  desired_count = 1
+  launch_type = "FARGATE"
+  
+  depends_on = [ aws_ecs_cluster.union_cluster ]
+}
